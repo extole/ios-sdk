69fc9aebfecc5be57f3ca821b2727fdf7d611e32
diftodi <diftodi@extole.com>
2022-06-03 17:42:09 +0300

    Removed unecessary files

--- Sources/ExtoleMobileSDK/ConditionType.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum ConditionType: String, Decodable {
-    case NOT_DEFINED
-    case EVENT
-}
--- Sources/ExtoleMobileSDK/impl/app/swiftui/ExtoleView.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftUI
-
-public struct ExtoleView: View {
-    @ObservedObject var view: ExtoleObservableUi
-    public var body: some View {
-        view.bodyContent
-    }
-}
